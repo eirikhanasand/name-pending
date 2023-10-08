4273e51d8e0ec3e45c455eb3174c6de9725ed771
eirikhanasand <eirik.hanasand@gmail.com>
2023-10-08 21:04:06 +0200

    Typo

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ client.once(Events.ClientReady, async message => {
     // client.application.commands.set([]) // Use to perge all inactive slash commands from Discord
     console.log('Ready!');
 
-    if (info.channelID && info.username && user.userID) {
+    if (info.channelID && info.username && info.userID) {
         const mID = "1160644160898994206"
         const msg = await message.channels.fetch(info.channelID).then(channel => channel.messages.fetch(mID))
         const embed = new EmbedBuilder()
