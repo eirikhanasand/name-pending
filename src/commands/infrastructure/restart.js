36fb0def23cebbccaf6fc116f2e7273c9a4720ec
eirikhanasand <eirik.hanasand@gmail.com>
2023-10-08 21:29:32 +0200

    Fixed bug

--- src/commands/infrastructure/restart.js
+++ src/commands/infrastructure/restart.js
@@ -122,8 +122,7 @@ async function restartBot(message, reason, branch) {
 
     const restart = [
         'cd ..',
-        `echo '#!/bin/bash\nrm -rf tekkom-bot\ngit clone -b ${branch} https://git.logntnu.no/tekkom/playground/tekkom-bot.git\ncd tekkom-bot\nnpm i && npm start'> temp.sh`,
-        `echo '{"token": "${config.token}", "clientId": "${config.clientId}", "guildId": "${config.guildId}", "docker_username": "${config.docker_username}", "docker_password": "${config.docker_password}"}' > tekkom-bot/config.json`,
+        `echo '#!/bin/bash\nrm -rf tekkom-bot\ngit clone -b ${branch} https://git.logntnu.no/tekkom/playground/tekkom-bot.git\ncd tekkom-bot\necho '{"token": "${config.token}", "clientId": "${config.clientId}", "guildId": "${config.guildId}", "docker_username": "${config.docker_username}", "docker_password": "${config.docker_password}"}' > config.json\nnpm i && npm start'> temp.sh`,
         `echo '{"branch": "${branch}", "reason": "${reason}", "channelID": "${message.channelId}", "username": "${message.user.username}", "userID": "${message.user.id}"}' > info.json`,
         'chmod +x temp.sh',
         './temp.sh'
