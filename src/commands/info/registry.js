90b66b81d49b99c2c1011195e7ef744bf45bc04b
eirikhanasand <eirik.hanasand@gmail.com>
2023-10-08 05:23:36 +0200

    Fixed formatting

--- src/commands/info/registry.js
+++ src/commands/info/registry.js
@@ -10,14 +10,17 @@ export async function execute(interaction) {
         .setColor("#fd8738")
         .setTimestamp()
         .addFields(
-            {name: "[OPTIONAL]", value: "docker service create --name <repository_name> --with-registry-auth registry.git.logntnu.no/tekkom/<repository>:latest"},
-            {name: "[DESCRIPTION]", value: "Creates a new registry item, use if the registry does not exist."},
-            {name: "[LOCAL]", value: "docker buildx build --platform linux/amd64,linux/arm64 --push -t registry.git.logntnu.no/<repository>:latest ."},
-            {name: "[DESCRIPTION]", value: "Builds a docker image for the Linux server we run our infrastructure on and pushes it to the registry. This makes the code available anywhere."},
-            {name: "[REMOTE]", value: "docker image pull registry.git.logntnu.no/<repository>:latest"},
-            {name: "[DESCRIPTION]", value: "Updates the image to the latest version in the registry."},
-            {name: "[REMOTE]", value: "docker service update --with-registry-auth --image registry.git.logntnu.no/tekkom/playground/tekkom-bot:latest tekkom-bot"},
-            {name: "[DESCRIPTION]", value: "Updates the docker service specified, effectively puts the latest version into production."},
+            {name: "**[OPTIONAL]**", value: "docker service create --name <repository_name> --with-registry-auth registry.git.logntnu.no/tekkom/<repository>:latest", inline: true},
+            {name: "**Description**", value: "Creates a new registry item, use if the registry does not exist.", inline: true},
+            {name: " ", value: " ", inline: false},
+            {name: "**[LOCAL]**", value: "docker buildx build --platform linux/amd64,linux/arm64 --push -t registry.git.logntnu.no/<repository>:latest .", inline: true},
+            {name: "**Description**", value: "Builds a docker image for the Linux server we run our infrastructure on and pushes it to the registry. This makes the code available anywhere.", inline: true},
+            {name: " ", value: " ", inline: false},
+            {name: "**[REMOTE]**", value: "docker image pull registry.git.logntnu.no/<repository>:latest", inline: true},
+            {name: "**Description**", value: "Updates the image to the latest version in the registry.", inline: true},
+            {name: " ", value: " ", inline: false},
+            {name: "**[REMOTE]**", value: "docker service update --with-registry-auth --image registry.git.logntnu.no/tekkom/playground/tekkom-bot:latest tekkom-bot", inline: true},
+            {name: "**Description**", value: "Updates the docker service specified, effectively puts the latest version into production.", inline: true},
         )
     await interaction.reply({ embeds: [embed]});
 }
