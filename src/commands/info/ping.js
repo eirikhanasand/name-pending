43f831db9dfb20b58c61b49e6f5398afb418faf9
eirikhanasand <eirik.hanasand@gmail.com>
2023-10-08 02:40:48 +0200

    Test

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -4,5 +4,5 @@ export const data = new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!');
 export async function execute(interaction) {
-    await interaction.reply('Pong!');
+    await interaction.reply('Pongeee!');
 }
