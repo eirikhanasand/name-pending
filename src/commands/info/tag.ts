2ea402d7a4e5ee727f61fa72be799fa22a05f55f
eirikhanasand <eirik.hanasand@gmail.com>
2024-05-14 17:58:09 +0200

    Added tag command

---
+++ src/commands/info/tag.ts
@@ -0,0 +1,8 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
+
+export const data = new SlashCommandBuilder()
+    .setName('tag')
+    .setDescription('Replies with info on how to tag an image')
+export async function execute(message: ChatInputCommandInteraction) {
+    await message.reply('```js\ngit add .\ngit commit -m "melding"\ngit tag -a <version> -m "melding" <commit_hash>\ngit push origin <version>\ngit push```')
+}
