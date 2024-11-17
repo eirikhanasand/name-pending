9afc615d42af616d2fffb3a9d3e0965bab8cf5ef
eirikhanasand <eirik.hanasand@gmail.com>
2024-11-17 21:48:26 +0100

    Version 1.9.11 - Updated dist

--- dist/constants.js
+++ dist/constants.js
@@ -1,6 +1,6 @@
 export const ticketIdPattern = /^ticket\d+$|^\d{5,}$/;
-// export const API = 'https://dizambee-api.login.no/api'
-export const API = "http://localhost:8080/api";
+export const API = 'https://dizambee-api.login.no/api';
+// export const API = "http://localhost:8080/api"
 export const ZAMMAD_URL = 'https://zammad.login.no/#ticket/zoom';
 export const DISCORD_URL = 'https://discord.com/channels';
 export const MAX_CHANNELS = 50;
