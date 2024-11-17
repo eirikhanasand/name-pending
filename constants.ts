99ee068472048318651015d2c26b690b5f972f69
eirikhanasand <eirik.hanasand@gmail.com>
2024-11-17 18:48:59 +0100

    Version 1.9.8 - Changed dizambee api to prod version

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 export const ticketIdPattern = /^ticket\d+$|^\d{5,}$/
-export const API = 'https://dev-dizambee-api.login.no/api'
+export const API = 'https://dizambee-api.login.no/api'
 // export const API = "http://localhost:8080/api"
 export const ZAMMAD_URL = 'https://zammad.login.no/#ticket/zoom'
 export const DISCORD_URL = 'https://discord.com/channels'
