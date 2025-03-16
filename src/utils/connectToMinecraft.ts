c8e805b898b84fb40b4c4ccc8bf9ec575fc77882
eirikhanasand <eirik.hanasand@gmail.com>
2025-03-16 05:38:12 +0100

    fixed bugs

---
+++ src/utils/connectToMinecraft.ts
@@ -0,0 +1,133 @@
+import { CacheType, ChatInputCommandInteraction, Message, TextChannel } from 'discord.js'
+import http from "http"
+import { Reaction } from 'discord.js'
+import config from './config.js'
+
+export default function connectToMinecraft(message: ChatInputCommandInteraction<CacheType>) {
+    // Filter to check that the author is not a bot to prevent an infinite loop
+    const filter = (response: Message) => !response.author.bot
+
+    // Message collector that collects all messages written in Discord
+    const textChannel = message.channel as TextChannel
+    const collector = textChannel.createMessageCollector({ filter })
+    
+    // Seperate collector that listens to reactions on all messages
+    const botMessageCollector = textChannel.createMessageCollector()
+    
+    collector?.on('collect', (m: Message) => {
+        post(`${m.author.username || m.author.globalName || m.author.id}: ${m.content}`)
+    })
+    
+    botMessageCollector?.on('collect', (m: Message) => {
+        // Listens for reactions for 1 minute on each message
+        const reactionCollector = m.createReactionCollector({ time: 60000 })
+        
+        // Logs the reaction interaction in game
+        reactionCollector.on('collect', (reaction: Reaction, user) => {
+            post(`${user.tag} reacted with ${reaction._emoji.name}`)
+        })
+    })
+    
+    
+    updatePlayerCount(message)
+    listen(message)
+}
+
+/**
+ * Posts the message from Discord on all servers
+ * @param {Discord_Message} message 
+ */
+function post(message: string) {
+    console.log("posting", message)
+    config.minecraft_servers.forEach((server) => {
+        fetch(`${config.minecraft_url}:${server.port}/${server.name}-message`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: message
+        })
+    })
+}
+
+/**
+ * Listens for content from Minecraft and posts it on Discord
+ * @param {Discord_Message} message 
+ */
+async function listen(message: ChatInputCommandInteraction) {
+    const textChannel = message.channel as TextChannel
+
+    const server = http.createServer((req) => {
+        if (req.headers['type'] === 'death') {
+            req.on('data', chunk => {
+                textChannel.send(`**${chunk.toString()}**`)
+            })
+        } else {
+            req.on('data', chunk => {
+                textChannel.send(chunk.toString())
+            })
+        }
+    })
+
+    server.listen(config.minecraft_port)
+}
+
+/**
+ * Updates the channel description of the channel tracking the Minecraft 
+ * chats with the player counts.
+ * @param {*} message Message object
+ */
+async function updatePlayerCount(message: ChatInputCommandInteraction) {
+    const channel = message.channel
+
+    // Runs once per 5 minutes as long as the chat is being mirrored
+    while (true) {
+        let prod = [] as string[]
+        let dev = [] as string[]
+        const maxWidth = 20
+        let players = ""
+        let topic = ""
+
+        await Promise.allSettled(config.minecraft_servers.map(async(server) => {
+            const response = await fetch(`${config.minecraft_url}:${server.port}/${server.name}-online`)
+            const data = await response.json()
+            
+            switch (server.name) {
+                case "prod": prod = data; break
+                case "dev": dev = data; break
+            }
+        }))
+
+        let playersProd = prod.length
+        let playersDev = dev.length
+
+        for (let i = 0; i < Math.max(playersProd, playersDev); i++) {
+            const playerProd = (prod[i] || "").substring(0, maxWidth)
+            const playerDev = (dev[i] || "").substring(0, maxWidth)
+            
+            const spacesProd = "\t".repeat(Math.max(0, (maxWidth - playerProd.length) / 4))
+            const spacesDev = "\t".repeat(Math.max(0, (maxWidth - playerDev.length) / 4))
+        
+            const tabs = Math.max(1, Math.floor((maxWidth - playerProd.length) / 4))
+            const tabCharacters = "\t".repeat(tabs)
+
+            players += `${playerProd}${spacesProd}${tabCharacters}${playerDev}${spacesDev}\n`
+        }
+
+        const online = prod.length + dev.length
+        const name = config.minecraft_servers[0].name
+
+        if (online) {
+            topic = `${name}. Online: ${online}\n${name} (${prod.length})\t\t\t\t   Dev (${dev.length})\n${players}`
+        } else {
+            topic = `${name}. There are no players online at this time.`
+        }
+        
+        if (channel && 'setTopic' in channel) {
+            channel?.setTopic(topic)
+        } else {
+            console.error("Failed to set topic in minecraft/chat.ts")
+        }
+
+        // Waits for 5 minutes (Discord rate limit)
+        await new Promise(resolve => setTimeout(resolve, 300000))
+    }
+}
