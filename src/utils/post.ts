e3fd22a1c91018b31f52ae3d38f3c8773881d544
eirikhanasand <eirik.hanasand@gmail.com>
2025-04-10 21:37:49 +0200

    removed console.log

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -6,7 +6,6 @@ import config from "./config.js"
 export default function post(message: string) {
     const url = config.minecraft_server_url
     const name = config.minecraft_server_name
-    console.log("posting", `${url}/${config.minecraft_server_name}-message`)
     fetch(`${url}/${name}-message`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
